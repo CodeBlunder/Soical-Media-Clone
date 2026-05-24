c0975570fe108d93ea177e9038f9392284415c72
CodeBlunder <vvce23cse0131@vvce.ac.in>
2026-05-24 21:39:47 +0530

    Solved minor Bugs

---
+++ dbase/versions/c2152ac2136e_create_posts_table.py
@@ -0,0 +1,28 @@
+"""create posts table
+
+Revision ID: c2152ac2136e
+Revises: 
+Create Date: 2026-05-24 19:22:14.971862
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'c2152ac2136e'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
